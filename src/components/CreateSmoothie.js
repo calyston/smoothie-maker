62bc6b1bb9b225ab5cfd52a9e43b2bc002c2e186
Christine <calyston.14@gmail.com>
2021-10-06 17:03:29 +0100

    feat: limit set on liquids

--- src/components/CreateSmoothie.js
+++ src/components/CreateSmoothie.js
@@ -22,8 +22,18 @@ const CreateSmoothie = () => {
       mango: false,
       pineapple: false,
       strawberries: false,
+    },
+    greens: {
+      kale: false,
+      spinach: false,
+    },
+    extras: {
+      ginger: false,
+      turmeric: false,
+      ice: false,
     }
-  });
+  }
+  );
 
   //Customer Name Input
   const inputCustomerName = (e) => {
@@ -32,15 +42,44 @@ const CreateSmoothie = () => {
 
   //Selecting Liquids to Add to Smoothie
   const liquidCheckBox = (e) => {
-    setSmoothieRecipe({
-      ...smoothieRecipe, liquids: { ...smoothieRecipe.liquids, [e.target.name]: !smoothieRecipe.liquids[e.target.name] }
-    })
+
+    //Check how many are selected
+    let selectedLiquids = 0;
+    let tempLiquids = smoothieRecipe.liquids;
+
+    tempLiquids = { ...tempLiquids, [e.target.name]: !tempLiquids[e.target.name] }
+
+    //Check how many are selected
+    for (const liquid in tempLiquids) {
+      selectedLiquids += tempLiquids[liquid]
+    }
+
+    //Set limit to amount of liquids- min 1 max 2
+    if (selectedLiquids <= 2) {
+      setSmoothieRecipe({
+        ...smoothieRecipe, liquids: { ...smoothieRecipe.liquids, [e.target.name]: !smoothieRecipe.liquids[e.target.name] }
+      })
+    }
+
   }
 
   //Selecting Fruits to Add to Smoothie
   const fruitsCheckBox = (e) => {
     setSmoothieRecipe({
       ...smoothieRecipe, fruits: { ...smoothieRecipe.fruits, [e.target.name]: !smoothieRecipe.fruits[e.target.name] }
+    })
+  }
+  //Selecting Greens to Add to Smoothie
+  const greensCheckBox = (e) => {
+    setSmoothieRecipe({
+      ...smoothieRecipe, greens: { ...smoothieRecipe.greens, [e.target.name]: !smoothieRecipe.greens[e.target.name] }
+    })
+  }
+
+  //Selecting Extra Ingredients to Add to Smoothie
+  const extrasCheckBox = (e) => {
+    setSmoothieRecipe({
+      ...smoothieRecipe, extras: { ...smoothieRecipe.extras, [e.target.name]: !smoothieRecipe.extras[e.target.name] }
     })
   }
 
@@ -60,7 +99,7 @@ const CreateSmoothie = () => {
             label="Milk"
             name="milk"
             type="checkbox"
-            value={smoothieRecipe.liquids.milk}
+            checked={smoothieRecipe.liquids.milk}
             onChange={liquidCheckBox}
           />
           <Form.Check
@@ -68,7 +107,7 @@ const CreateSmoothie = () => {
             label="Oat Milk"
             name="oatMilk"
             type="checkbox"
-            value={smoothieRecipe.liquids.oatMilk}
+            checked={smoothieRecipe.liquids.oatMilk}
             onChange={liquidCheckBox}
           />
           <Form.Check
@@ -76,7 +115,7 @@ const CreateSmoothie = () => {
             label="Soy Milk"
             name="soyMilk"
             type="checkbox"
-            value={smoothieRecipe.liquids.soyMilk}
+            checked={smoothieRecipe.liquids.soyMilk}
             onChange={liquidCheckBox}
           />
           <Form.Check
@@ -92,7 +131,7 @@ const CreateSmoothie = () => {
             label="Orange Juice"
             name="orangeJuice"
             type="checkbox"
-            value={smoothieRecipe.liquids.orangeJuice}
+            checked={smoothieRecipe.liquids.orangeJuice}
             onChange={liquidCheckBox}
           />
         </Form.Group>
@@ -104,7 +143,7 @@ const CreateSmoothie = () => {
             label="Apple"
             name="apple"
             type="checkbox"
-            value={smoothieRecipe.liquids.apple}
+            checked={smoothieRecipe.fruits.apple}
             onChange={fruitsCheckBox}
           />
           <Form.Check
@@ -112,7 +151,7 @@ const CreateSmoothie = () => {
             label="Avocado"
             name="avocado"
             type="checkbox"
-            value={smoothieRecipe.liquids.avocado}
+            checked={smoothieRecipe.fruits.avocado}
             onChange={fruitsCheckBox}
           />
           <Form.Check
@@ -120,7 +159,7 @@ const CreateSmoothie = () => {
             label="Banana"
             name="banana"
             type="checkbox"
-            value={smoothieRecipe.liquids.banana}
+            checked={smoothieRecipe.fruits.banana}
             onChange={fruitsCheckBox}
           />
           <Form.Check
@@ -128,7 +167,7 @@ const CreateSmoothie = () => {
             label="Blueberries"
             name="blueberries"
             type="checkbox"
-            value={smoothieRecipe.liquids.blueberries}
+            checked={smoothieRecipe.fruits.blueberries}
             onChange={fruitsCheckBox}
           />
           <Form.Check
@@ -136,7 +175,7 @@ const CreateSmoothie = () => {
             label="Mango"
             name="mango"
             type="checkbox"
-            value={smoothieRecipe.liquids.mango}
+            checked={smoothieRecipe.fruits.mango}
             onChange={fruitsCheckBox}
           />
           <Form.Check
@@ -144,7 +183,7 @@ const CreateSmoothie = () => {
             label="Pineapple"
             name="pineapple"
             type="checkbox"
-            value={smoothieRecipe.liquids.pineapple}
+            checked={smoothieRecipe.fruits.pineapple}
             onChange={fruitsCheckBox}
           />
           <Form.Check
@@ -152,8 +191,56 @@ const CreateSmoothie = () => {
             label="Strawberries"
             name="strawberries"
             type="checkbox"
-            value={smoothieRecipe.liquids.strawberries}
+            checked={smoothieRecipe.fruits.strawberries}
             onChange={fruitsCheckBox}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="Greens">
+          <h4>Greens</h4>
+          <Form.Check
+            inline
+            label="Kale"
+            name="kale"
+            type="checkbox"
+            checked={smoothieRecipe.greens.kale}
+            onChange={greensCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Spinach"
+            name="spinach"
+            type="checkbox"
+            checked={smoothieRecipe.greens.spinach}
+            onChange={greensCheckBox}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="Extras">
+          <h4>Extras</h4>
+          <Form.Check
+            inline
+            label="Ginger"
+            name="ginger"
+            type="checkbox"
+            checked={smoothieRecipe.extras.ginger}
+            onChange={extrasCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Turmeric"
+            name="turmeric"
+            type="checkbox"
+            checked={smoothieRecipe.extras.turmeric}
+            onChange={extrasCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Ice"
+            name="ice"
+            type="checkbox"
+            checked={smoothieRecipe.extras.ice}
+            onChange={extrasCheckBox}
           />
         </Form.Group>
 
