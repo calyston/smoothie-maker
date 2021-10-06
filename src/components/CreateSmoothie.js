254649b478149e8cd9a5d7ecdd588d430d052cac
Christine <calyston.14@gmail.com>
2021-10-06 15:52:41 +0100

    form checkboxes added for fruits!

--- src/components/CreateSmoothie.js
+++ src/components/CreateSmoothie.js
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button';
 
 const CreateSmoothie = () => {
 
-  const [smoothieInfo, setSmoothieInfo] = useState({
+  const [smoothieRecipe, setSmoothieRecipe] = useState({
     name: '',
     liquids: {
       milk: false,
@@ -13,25 +13,43 @@ const CreateSmoothie = () => {
       soyMilk: false,
       greenTea: false,
       orangeJuice: false,
+    },
+    fruits: {
+      apple: false,
+      avocado: false,
+      banana: false,
+      blueberries: false,
+      mango: false,
+      pineapple: false,
+      strawberries: false,
     }
   });
 
   //Customer Name Input
   const inputCustomerName = (e) => {
-    setSmoothieInfo({ name: e.target.value })
+    setSmoothieRecipe({ ...smoothieRecipe, name: e.target.value })
   };
 
   //Selecting Liquids to Add to Smoothie
-  // const liquidCheckBox = (e) => {
+  const liquidCheckBox = (e) => {
+    setSmoothieRecipe({
+      ...smoothieRecipe, liquids: { ...smoothieRecipe.liquids, [e.target.name]: !smoothieRecipe.liquids[e.target.name] }
+    })
+  }
 
-  // }
+  //Selecting Fruits to Add to Smoothie
+  const fruitsCheckBox = (e) => {
+    setSmoothieRecipe({
+      ...smoothieRecipe, fruits: { ...smoothieRecipe.fruits, [e.target.name]: !smoothieRecipe.fruits[e.target.name] }
+    })
+  }
 
   return (
     <div className="form-wrapper">
       <Form>
         <Form.Group controlId="Name">
           <Form.Label>Customer Name:</Form.Label>
-          <Form.Control type="text" value={smoothieInfo.name} onChange={inputCustomerName} />
+          <Form.Control type="text" value={smoothieRecipe.name} onChange={inputCustomerName} />
         </Form.Group>
 
         <h2>Create Your Smoothie!</h2>
@@ -42,30 +60,100 @@ const CreateSmoothie = () => {
             label="Milk"
             name="milk"
             type="checkbox"
-          // value={smoothieInfo.liquids.milk}
-          // onChange={liquidCheckBox}
+            value={smoothieRecipe.liquids.milk}
+            onChange={liquidCheckBox}
           />
           <Form.Check
             inline
             label="Oat Milk"
+            name="oatMilk"
             type="checkbox"
-            name="group1"
+            value={smoothieRecipe.liquids.oatMilk}
+            onChange={liquidCheckBox}
           />
           <Form.Check
             inline
             label="Soy Milk"
+            name="soyMilk"
             type="checkbox"
-            name="group1"
-          />          <Form.Check
+            value={smoothieRecipe.liquids.soyMilk}
+            onChange={liquidCheckBox}
+          />
+          <Form.Check
             inline
             label="Green Tea"
+            name="greenTea"
             type="checkbox"
-            name="group1"
-          />          <Form.Check
+            value={smoothieRecipe.liquids.greenTea}
+            onChange={liquidCheckBox}
+          />
+          <Form.Check
             inline
             label="Orange Juice"
+            name="orangeJuice"
             type="checkbox"
-            name="group1"
+            value={smoothieRecipe.liquids.orangeJuice}
+            onChange={liquidCheckBox}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="Fruits">
+          <h4>Fruits</h4>
+          <Form.Check
+            inline
+            label="Apple"
+            name="apple"
+            type="checkbox"
+            value={smoothieRecipe.liquids.apple}
+            onChange={fruitsCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Avocado"
+            name="avocado"
+            type="checkbox"
+            value={smoothieRecipe.liquids.avocado}
+            onChange={fruitsCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Banana"
+            name="banana"
+            type="checkbox"
+            value={smoothieRecipe.liquids.banana}
+            onChange={fruitsCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Blueberries"
+            name="blueberries"
+            type="checkbox"
+            value={smoothieRecipe.liquids.blueberries}
+            onChange={fruitsCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Mango"
+            name="mango"
+            type="checkbox"
+            value={smoothieRecipe.liquids.mango}
+            onChange={fruitsCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Pineapple"
+            name="pineapple"
+            type="checkbox"
+            value={smoothieRecipe.liquids.pineapple}
+            onChange={fruitsCheckBox}
+          />
+          <Form.Check
+            inline
+            label="Strawberries"
+            name="strawberries"
+            type="checkbox"
+            value={smoothieRecipe.liquids.strawberries}
+            onChange={fruitsCheckBox}
           />
         </Form.Group>
 
@@ -74,40 +162,6 @@ const CreateSmoothie = () => {
         </Button>
       </Form>
     </div >
-  )
-  return (
-    <div className="form-wrapper">
-      <Form>
-        <h3>Create Your Smoothie!</h3>
-        <Form.Group controlId="Liquid">
-          <Form.Label>Liquid</Form.Label>
-          <Button size="lg" block="block">
-            Milk
-          </Button>
-          <Button size="lg" block="block">
-            Oat Milk
-          </Button>
-          <Button size="lg" block="block">
-            Soy Milk
-          </Button>
-          <Button size="lg" block="block">
-            Green Tea
-          </Button>
-        </Form.Group>
-
-        <Form.Group controlId="Fruit">
-          <Form.Label>Fruit</Form.Label>
-        </Form.Group>
-
-        <Form.Group controlId="Extra">
-          <Form.Label>Extra</Form.Label>
-        </Form.Group>
-
-        <Button size="lg" block="block" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </div>
   )
 }
 
